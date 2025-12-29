f2d0852eb71d93cc5adda5d72e3d593ab6f701e1
GitHub Action <action@github.com>
2025-12-29 05:30:16 +0000

    Template cleanup

--- src/main/kotlin/com/github/zhouxintim/xihengideatool/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/zhouxintim/xihengideatool/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.zhouxintim.xihengideatool.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
